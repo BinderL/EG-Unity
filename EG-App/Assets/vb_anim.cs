5c4de80dddc10392473dd195b044977156ec7469
BrunoSouchu <b.souchu@hotmail.com>
2018-01-25 15:05:48 +0100

    boutton opérationnel 1 target
    This reverts commit 967960f8879c9fc5a934bc098d9ea3f2109434c1.

---
+++ EG-App/Assets/vb_anim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb_anim : MonoBehaviour, IVirtualButtonEventHandler {
+
+	public GameObject vbBtnObj;
+	public Animator ballAni;
+
+	// Use this for initialization
+	void Start () {
+		vbBtnObj = GameObject.Find ("Natoobtn");
+		vbBtnObj.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+		ballAni.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb){
+		ballAni.Play ("ball_animation");
+		Debug.Log ("ON");
+	}
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb){
+		ballAni.Play ("none");
+		Debug.Log ("OFF");
+	}
+}
