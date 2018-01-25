ee9625f8b314f317bccd9a32e26c416051b515e5
BrunoSouchu <b.souchu@hotmail.com>
2018-01-25 15:42:18 +0100

    2targets 2btn 2=OK

---
+++ EG-App/Assets/vb_anim_cube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb_anim_cube : MonoBehaviour, IVirtualButtonEventHandler {
+
+	public GameObject vbBtnObjAstro;
+	public Animator cubeAni;
+
+
+	void Start () {
+		vbBtnObjAstro = GameObject.Find ("Astrobtn");
+		vbBtnObjAstro.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+		cubeAni.GetComponent<Animator> ();
+	}
+		
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb){
+		cubeAni.Play ("cube_animation");
+		Debug.Log ("ON_CUBE");
+	}
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb){
+		cubeAni.Play ("none");
+		Debug.Log ("OFF_CUBE");
+	}
+}
