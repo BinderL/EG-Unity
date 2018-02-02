e00596b0b9868c76041f1d77bec464a057fb09e6
BinderL <binders@laposte.net>
2018-02-02 01:32:57 +0100

    Add music
    +music controller

---
+++ EG-App/Assets/AudioControler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControler : MonoBehaviour {
+
+    public vb_anim_leaf chest;
+    private AudioSource MusicControl = new AudioSource();
+    public AudioClip Zelda;
+
+	// Use this for initialization
+	void Start () {
+  
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (chest.active==true)
+        {
+            MusicControl.PlayOneShot(Zelda);
+            Debug.Log("musique playing");
+        }
+        else
+            MusicControl.Pause();
+    }
+}
