699343a1e7d43435bd982d3bb71961187b680bd1
BinderL <binders@laposte.net>
2018-02-22 21:42:34 +0100

    commit UI (2)
    -> improve Ui

---
+++ EG-App/Assets/LoadOnEnter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnEnter : StateMachineBehaviour
+{
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        SceneManager.LoadScene(2);
+    }
+}
